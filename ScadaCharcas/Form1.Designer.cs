4d8d54e2ce21c39e25e3a729ba5232fd153a7429
RoblesDiego <kael.r1991@gmail.com>
2017-03-02 12:05:52 -0400

    Diseño base

---
+++ ScadaCharcas/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace ScadaCharcas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contenedor11 = new ScadaControles.Contenedor1();
+            this.contenedor12 = new ScadaControles.Contenedor1();
+            this.banda_transportadora1 = new ScadaControles.Banda_transportadora();
+            this.tanque21 = new ScadaControles.Tanque2();
+            this.contenedor13 = new ScadaControles.Contenedor1();
+            this.motor1 = new ScadaControles.Motor();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PB_apertura3 = new System.Windows.Forms.PictureBox();
+            this.PB_apertura2 = new System.Windows.Forms.PictureBox();
+            this.PB_apertura1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Time_control = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.PB_apertura3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_apertura2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_apertura1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contenedor11
+            // 
+            this.contenedor11.Location = new System.Drawing.Point(839, 195);
+            this.contenedor11.Name = "contenedor11";
+            this.contenedor11.Size = new System.Drawing.Size(176, 167);
+            this.contenedor11.TabIndex = 1;
+            // 
+            // contenedor12
+            // 
+            this.contenedor12.Location = new System.Drawing.Point(1012, 195);
+            this.contenedor12.Name = "contenedor12";
+            this.contenedor12.Size = new System.Drawing.Size(177, 167);
+            this.contenedor12.TabIndex = 2;
+            // 
+            // banda_transportadora1
+            // 
+            this.banda_transportadora1.Location = new System.Drawing.Point(121, 523);
+            this.banda_transportadora1.Name = "banda_transportadora1";
+            this.banda_transportadora1.Size = new System.Drawing.Size(443, 61);
+            this.banda_transportadora1.TabIndex = 3;
+            // 
+            // tanque21
+            // 
+            this.tanque21.Location = new System.Drawing.Point(1137, 12);
+            this.tanque21.Name = "tanque21";
+            this.tanque21.Size = new System.Drawing.Size(244, 214);
+            this.tanque21.TabIndex = 4;
+            // 
+            // contenedor13
+            // 
+            this.contenedor13.Location = new System.Drawing.Point(917, 359);
+            this.contenedor13.Name = "contenedor13";
+            this.contenedor13.Size = new System.Drawing.Size(177, 167);
+            this.contenedor13.TabIndex = 6;
+            // 
+            // motor1
+            // 
+            this.motor1.Location = new System.Drawing.Point(0, 509);
+            this.motor1.Name = "motor1";
+            this.motor1.Size = new System.Drawing.Size(126, 84);
+            this.motor1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 50.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(363, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(551, 69);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Control De Proceso";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1179, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Tanque de agua";
+            // 
+            // PB_apertura3
+            // 
+            this.PB_apertura3.Image = global::ScadaCharcas.Properties.Resources.cuadro_rojo;
+            this.PB_apertura3.Location = new System.Drawing.Point(984, 517);
+            this.PB_apertura3.Name = "PB_apertura3";
+            this.PB_apertura3.Size = new System.Drawing.Size(42, 19);
+            this.PB_apertura3.TabIndex = 12;
+            this.PB_apertura3.TabStop = false;
+            // 
+            // PB_apertura2
+            // 
+            this.PB_apertura2.Image = global::ScadaCharcas.Properties.Resources.cuadro_rojo;
+            this.PB_apertura2.Location = new System.Drawing.Point(1079, 343);
+            this.PB_apertura2.Name = "PB_apertura2";
+            this.PB_apertura2.Size = new System.Drawing.Size(42, 19);
+            this.PB_apertura2.TabIndex = 11;
+            this.PB_apertura2.TabStop = false;
+            // 
+            // PB_apertura1
+            // 
+            this.PB_apertura1.Image = global::ScadaCharcas.Properties.Resources.cuadro_rojo;
+            this.PB_apertura1.Location = new System.Drawing.Point(904, 343);
+            this.PB_apertura1.Name = "PB_apertura1";
+            this.PB_apertura1.Size = new System.Drawing.Size(42, 19);
+            this.PB_apertura1.TabIndex = 10;
+            this.PB_apertura1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ScadaCharcas.Properties.Resources.contenedor3;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 359);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(293, 144);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ScadaCharcas.Properties.Resources.tornillo_sin_fin_a_colores;
+            this.pictureBox2.Location = new System.Drawing.Point(375, 208);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(596, 309);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1362, 650);
+            this.Controls.Add(this.PB_apertura3);
+            this.Controls.Add(this.PB_apertura2);
+            this.Controls.Add(this.PB_apertura1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.contenedor13);
+            this.Controls.Add(this.banda_transportadora1);
+            this.Controls.Add(this.contenedor12);
+            this.Controls.Add(this.contenedor11);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.tanque21);
+            this.Controls.Add(this.motor1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PB_apertura3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_apertura2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_apertura1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private ScadaControles.Contenedor1 contenedor11;
+        private ScadaControles.Contenedor1 contenedor12;
+        private ScadaControles.Banda_transportadora banda_transportadora1;
+        private ScadaControles.Tanque2 tanque21;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private ScadaControles.Contenedor1 contenedor13;
+        private ScadaControles.Motor motor1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox PB_apertura1;
+        private System.Windows.Forms.PictureBox PB_apertura2;
+        private System.Windows.Forms.PictureBox PB_apertura3;
+        private System.Windows.Forms.Timer Time_control;
+
+    }
+}
+
