de3f144ccd03b46d3b7671ab308fc144fc123685
RoblesDiego <kael.r1991@gmail.com>
2017-03-20 13:22:13 -0400

    Primer etapa
    probando lectura y escritura

--- ScadaCharcas/Form1.Designer.cs
+++ ScadaCharcas/Form1.Designer.cs
@@ -34,7 +34,6 @@
             this.banda_transportadora1 = new ScadaControles.Banda_transportadora();
             this.tanque21 = new ScadaControles.Tanque2();
             this.contenedor13 = new ScadaControles.Contenedor1();
-            this.motor1 = new ScadaControles.Motor();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.PB_apertura3 = new System.Windows.Forms.PictureBox();
@@ -42,7 +41,21 @@
             this.PB_apertura1 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.Time_control = new System.Windows.Forms.Timer(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_tm1 = new System.Windows.Forms.TextBox();
+            this.btn_confTm1 = new System.Windows.Forms.Button();
+            this.lbl_time1 = new System.Windows.Forms.Label();
+            this.lbl_time2 = new System.Windows.Forms.Label();
+            this.btn_confTm2 = new System.Windows.Forms.Button();
+            this.txt_tm2 = new System.Windows.Forms.TextBox();
+            this.btn_Conectar = new System.Windows.Forms.Button();
+            this.btn_desconectar = new System.Windows.Forms.Button();
+            this.btn_iniciarMonitoreo = new System.Windows.Forms.Button();
+            this.btn_finMonitoreo = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.btn_salir = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.PB_apertura3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.PB_apertura2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.PB_apertura1)).BeginInit();
@@ -66,9 +79,9 @@
             // 
             // banda_transportadora1
             // 
-            this.banda_transportadora1.Location = new System.Drawing.Point(121, 523);
+            this.banda_transportadora1.Location = new System.Drawing.Point(76, 523);
             this.banda_transportadora1.Name = "banda_transportadora1";
-            this.banda_transportadora1.Size = new System.Drawing.Size(443, 61);
+            this.banda_transportadora1.Size = new System.Drawing.Size(433, 61);
             this.banda_transportadora1.TabIndex = 3;
             // 
             // tanque21
@@ -84,13 +97,6 @@
             this.contenedor13.Name = "contenedor13";
             this.contenedor13.Size = new System.Drawing.Size(177, 167);
             this.contenedor13.TabIndex = 6;
-            // 
-            // motor1
-            // 
-            this.motor1.Location = new System.Drawing.Point(0, 509);
-            this.motor1.Name = "motor1";
-            this.motor1.Size = new System.Drawing.Size(126, 84);
-            this.motor1.TabIndex = 7;
             // 
             // label1
             // 
@@ -142,7 +148,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::ScadaCharcas.Properties.Resources.contenedor3;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 359);
+            this.pictureBox1.Location = new System.Drawing.Point(76, 359);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(293, 144);
             this.pictureBox1.TabIndex = 0;
@@ -157,11 +163,157 @@
             this.pictureBox2.TabIndex = 5;
             this.pictureBox2.TabStop = false;
             // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Tiempo de llenado";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(61, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Tiempo de llenado";
+            // 
+            // txt_tm1
+            // 
+            this.txt_tm1.Location = new System.Drawing.Point(64, 153);
+            this.txt_tm1.Name = "txt_tm1";
+            this.txt_tm1.Size = new System.Drawing.Size(32, 20);
+            this.txt_tm1.TabIndex = 15;
+            // 
+            // btn_confTm1
+            // 
+            this.btn_confTm1.Enabled = false;
+            this.btn_confTm1.Location = new System.Drawing.Point(102, 151);
+            this.btn_confTm1.Name = "btn_confTm1";
+            this.btn_confTm1.Size = new System.Drawing.Size(40, 23);
+            this.btn_confTm1.TabIndex = 16;
+            this.btn_confTm1.Text = "Conf.";
+            this.btn_confTm1.UseVisualStyleBackColor = true;
+            this.btn_confTm1.Click += new System.EventHandler(this.btn_confTm1_Click);
+            // 
+            // lbl_time1
+            // 
+            this.lbl_time1.AutoSize = true;
+            this.lbl_time1.Location = new System.Drawing.Point(75, 135);
+            this.lbl_time1.Name = "lbl_time1";
+            this.lbl_time1.Size = new System.Drawing.Size(48, 13);
+            this.lbl_time1.TabIndex = 17;
+            this.lbl_time1.Text = "lbl_time1";
+            // 
+            // lbl_time2
+            // 
+            this.lbl_time2.AutoSize = true;
+            this.lbl_time2.Location = new System.Drawing.Point(75, 193);
+            this.lbl_time2.Name = "lbl_time2";
+            this.lbl_time2.Size = new System.Drawing.Size(48, 13);
+            this.lbl_time2.TabIndex = 20;
+            this.lbl_time2.Text = "lbl_time2";
+            // 
+            // btn_confTm2
+            // 
+            this.btn_confTm2.Enabled = false;
+            this.btn_confTm2.Location = new System.Drawing.Point(102, 209);
+            this.btn_confTm2.Name = "btn_confTm2";
+            this.btn_confTm2.Size = new System.Drawing.Size(40, 23);
+            this.btn_confTm2.TabIndex = 19;
+            this.btn_confTm2.Text = "Conf.";
+            this.btn_confTm2.UseVisualStyleBackColor = true;
+            this.btn_confTm2.Click += new System.EventHandler(this.btn_confTm2_Click);
+            // 
+            // txt_tm2
+            // 
+            this.txt_tm2.Location = new System.Drawing.Point(64, 211);
+            this.txt_tm2.Name = "txt_tm2";
+            this.txt_tm2.Size = new System.Drawing.Size(32, 20);
+            this.txt_tm2.TabIndex = 18;
+            // 
+            // btn_Conectar
+            // 
+            this.btn_Conectar.Location = new System.Drawing.Point(38, 9);
+            this.btn_Conectar.Name = "btn_Conectar";
+            this.btn_Conectar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Conectar.TabIndex = 21;
+            this.btn_Conectar.Text = "Conectar";
+            this.btn_Conectar.UseVisualStyleBackColor = true;
+            this.btn_Conectar.Click += new System.EventHandler(this.btn_Conectar_Click);
+            // 
+            // btn_desconectar
+            // 
+            this.btn_desconectar.Enabled = false;
+            this.btn_desconectar.Location = new System.Drawing.Point(134, 9);
+            this.btn_desconectar.Name = "btn_desconectar";
+            this.btn_desconectar.Size = new System.Drawing.Size(82, 23);
+            this.btn_desconectar.TabIndex = 22;
+            this.btn_desconectar.Text = "Desconectar";
+            this.btn_desconectar.UseVisualStyleBackColor = true;
+            this.btn_desconectar.Click += new System.EventHandler(this.btn_desconectar_Click);
+            // 
+            // btn_iniciarMonitoreo
+            // 
+            this.btn_iniciarMonitoreo.Enabled = false;
+            this.btn_iniciarMonitoreo.Location = new System.Drawing.Point(38, 39);
+            this.btn_iniciarMonitoreo.Name = "btn_iniciarMonitoreo";
+            this.btn_iniciarMonitoreo.Size = new System.Drawing.Size(75, 40);
+            this.btn_iniciarMonitoreo.TabIndex = 23;
+            this.btn_iniciarMonitoreo.Text = "Iniciar Monitoreo";
+            this.btn_iniciarMonitoreo.UseVisualStyleBackColor = true;
+            this.btn_iniciarMonitoreo.Click += new System.EventHandler(this.btn_iniciarMonitoreo_Click);
+            // 
+            // btn_finMonitoreo
+            // 
+            this.btn_finMonitoreo.Enabled = false;
+            this.btn_finMonitoreo.Location = new System.Drawing.Point(134, 39);
+            this.btn_finMonitoreo.Name = "btn_finMonitoreo";
+            this.btn_finMonitoreo.Size = new System.Drawing.Size(70, 40);
+            this.btn_finMonitoreo.TabIndex = 24;
+            this.btn_finMonitoreo.Text = "Fin Monitoreo";
+            this.btn_finMonitoreo.UseVisualStyleBackColor = true;
+            this.btn_finMonitoreo.Click += new System.EventHandler(this.btn_finMonitoreo_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Location = new System.Drawing.Point(226, 38);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(70, 40);
+            this.btn_salir.TabIndex = 25;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1362, 650);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_finMonitoreo);
+            this.Controls.Add(this.btn_iniciarMonitoreo);
+            this.Controls.Add(this.btn_desconectar);
+            this.Controls.Add(this.btn_Conectar);
+            this.Controls.Add(this.lbl_time2);
+            this.Controls.Add(this.btn_confTm2);
+            this.Controls.Add(this.txt_tm2);
+            this.Controls.Add(this.lbl_time1);
+            this.Controls.Add(this.btn_confTm1);
+            this.Controls.Add(this.txt_tm1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.PB_apertura3);
             this.Controls.Add(this.PB_apertura2);
             this.Controls.Add(this.PB_apertura1);
@@ -174,7 +326,6 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.tanque21);
-            this.Controls.Add(this.motor1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.PB_apertura3)).EndInit();
@@ -196,13 +347,26 @@
         private ScadaControles.Tanque2 tanque21;
         private System.Windows.Forms.PictureBox pictureBox2;
         private ScadaControles.Contenedor1 contenedor13;
-        private ScadaControles.Motor motor1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox PB_apertura1;
         private System.Windows.Forms.PictureBox PB_apertura2;
         private System.Windows.Forms.PictureBox PB_apertura3;
-        private System.Windows.Forms.Timer Time_control;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_tm1;
+        private System.Windows.Forms.Button btn_confTm1;
+        private System.Windows.Forms.Label lbl_time1;
+        private System.Windows.Forms.Label lbl_time2;
+        private System.Windows.Forms.Button btn_confTm2;
+        private System.Windows.Forms.TextBox txt_tm2;
+        private System.Windows.Forms.Button btn_Conectar;
+        private System.Windows.Forms.Button btn_desconectar;
+        private System.Windows.Forms.Button btn_iniciarMonitoreo;
+        private System.Windows.Forms.Button btn_finMonitoreo;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button btn_salir;
 
     }
 }
--- ScadaCharcas/Form1.cs
+++ ScadaCharcas/Form1.cs
@@ -7,14 +7,142 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenOPC;
+using EasyModbus;
 
 namespace ScadaCharcas
 {
     public partial class Form1 : Form
     {
+        ModbusClient modbusClient;
         public Form1()
         {
             InitializeComponent();
+        }
+        public static bool[] leer_M;
+        public static int[] leer_MW;
+        public static bool[] escribir_M;
+        public static int[] escribir_MW;
+
+        public string confTime1;
+        public int SetTime1;
+        public string confTime2;
+        public int SetTime2;
+        private void conectarPLC()
+        {
+            modbusClient = new ModbusClient("192.168.0.100", 502); //dirección estática del plc
+            modbusClient.Connect();
+            MessageBox.Show("Conexión establecida.");
+            timer1.Start();
+        }
+        private void iniciarCaptura()
+        {
+            timer2.Start();
+        }
+        private void detenerLectura()
+        {
+            timer2.Stop();
+        }
+        private void btn_Conectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.conectarPLC();
+
+                btn_Conectar.Enabled = false;
+                btn_desconectar.Enabled = true;
+                btn_iniciarMonitoreo.Enabled = true;
+                btn_finMonitoreo.Enabled = false;
+                btn_salir.Enabled = false;
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void btn_desconectar_Click(object sender, EventArgs e)
+        {
+            btn_Conectar.Enabled = true;
+            btn_desconectar.Enabled = false;
+            btn_iniciarMonitoreo.Enabled = false;
+            btn_finMonitoreo.Enabled = false;
+            btn_salir.Enabled = true;
+            modbusClient.Disconnect();
+            MessageBox.Show("Conexion cerrada");
+            timer1.Stop(); 
+        }
+
+        private void btn_iniciarMonitoreo_Click(object sender, EventArgs e)
+        {
+            btn_Conectar.Enabled = false;
+            btn_desconectar.Enabled = false;
+            btn_iniciarMonitoreo.Enabled = false;
+            btn_finMonitoreo.Enabled = true;
+            btn_confTm1.Enabled = true;
+            btn_confTm2.Enabled = true;
+            this.iniciarCaptura();
+        }
+
+        private void btn_finMonitoreo_Click(object sender, EventArgs e)
+        {
+            this.detenerLectura();
+
+            btn_Conectar.Enabled = false;
+            btn_desconectar.Enabled = true;
+            btn_iniciarMonitoreo.Enabled = true;
+            btn_finMonitoreo.Enabled = false;
+            btn_confTm1.Enabled = false;
+            btn_confTm2.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Start();
+            try
+            {
+                leer_M = modbusClient.ReadCoils(0, 100);
+                leer_MW = modbusClient.ReadHoldingRegisters(0, 100);
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            timer2.Start();
+            try { 
+            lbl_time1.Text = leer_MW[0].ToString();
+            lbl_time2.Text = leer_MW[2].ToString();
+                
+                }
+            catch { }
+
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_confTm1_Click(object sender, EventArgs e)
+        {
+            confTime1 = txt_tm1.Text;
+            SetTime1 = Int32.Parse(confTime1);
+            modbusClient.WriteSingleRegister(0, SetTime1);
+            txt_tm1.Clear();
+
+          
+        }
+
+        private void btn_confTm2_Click(object sender, EventArgs e)
+        {
+            confTime2 = txt_tm2.Text;
+            SetTime2 = Int32.Parse(confTime2);
+            modbusClient.WriteSingleRegister(2, SetTime2);
+            txt_tm2.Clear();
         }
     }
 }
